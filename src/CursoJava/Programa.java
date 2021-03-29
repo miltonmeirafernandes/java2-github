690111084042799f83d9bb2d614745ef7b82f706
Milton Meira <milton.mf@hotmail.com>
2021-03-29 19:46:00 -0300

    Mensagem de Boa madrugada e eliminaçao de linhas vazias

--- src/CursoJava/Programa.java
+++ src/CursoJava/Programa.java
@@ -4,11 +4,9 @@ public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		System.out.println("Bom dia");
 		System.out.println("Bom Tarde");
 		System.out.println("Bom Noite");
-
+		System.out.println("Bom Madrugada");
 	}
-
 }
